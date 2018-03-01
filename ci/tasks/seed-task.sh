ea2326596d22f82350b6ff13095065354157bd32
Kyle Lydic <KyleLydic@gmail.com>
2018-02-28 21:36:25 -0600

    Y u no param?

--- ci/tasks/seed-task.sh
+++ ci/tasks/seed-task.sh
@@ -2,6 +2,9 @@
 
 set -e -x
 
+echo "Trying to get one of these params to work..."
+echo "${concourse-url}"
+
 mv ./fly-release/fly_linux_amd64 ./fly
 chmod +x ./fly
 ./fly -t local login -c ${concourse-url} -n ${concourse-team} -u ${concourse-username} -p ${concourse-password}
