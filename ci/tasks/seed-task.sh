a1a3d3e9978a635b579a43d554a8d769e7267cf2
Kyle Lydic <KyleLydic@gmail.com>
2018-02-28 21:05:15 -0600

    First try at seed job

---
+++ ci/tasks/seed-task.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e -x
+
+mv ./fly-release/fly_linux_amd64 ./fly
+chmod +x ./fly
+./fly -t local login -c {{concourse-url}} -n {{concourse-team}} -u {{concourse-username}} -p {{concourse-password}}
+./fly -t local sync
+./fly -t local set-pipeline -p "hello-world" -c ./source-code/ci/pipeline.yml -n
