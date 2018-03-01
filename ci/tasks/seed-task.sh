7ab3a7323777cd4e25b229d76c8e571513ce4c60
Kyle Lydic <KyleLydic@gmail.com>
2018-02-28 21:28:44 -0600

    Welformed yaml is important...

--- ci/tasks/seed-task.sh
+++ ci/tasks/seed-task.sh
@@ -4,6 +4,6 @@ set -e -x
 
 mv ./fly-release/fly_linux_amd64 ./fly
 chmod +x ./fly
-./fly -t local login -c {{concourse-url}} -n {{concourse-team}} -u {{concourse-username}} -p {{concourse-password}}
+./fly -t local login -c ${concourse-url} -n ${concourse-team} -u ${concourse-username}} -p {{concourse-password}
 ./fly -t local sync
 ./fly -t local set-pipeline -p "hello-world" -c ./source-code/ci/pipeline.yml -n
