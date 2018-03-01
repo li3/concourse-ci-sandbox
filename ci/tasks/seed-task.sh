4acebb3ecf1fd38cb027fb4bfbdc351d52cb8b49
Kyle Lydic <KyleLydic@gmail.com>
2018-02-28 22:08:23 -0600

    Try passing params through the pipeline to task file

--- ci/tasks/seed-task.sh
+++ ci/tasks/seed-task.sh
@@ -2,8 +2,11 @@
 
 set -e -x
 
+echo "Trying to get one of these params to work..."
+echo "${concourse-url}"
+
 mv ./fly-release/fly_linux_amd64 ./fly
 chmod +x ./fly
-./fly -t local login -c $3 -n $4 -u $1 -p $2
+./fly -t local login -c ${concourse-url} -n ${concourse-team} -u ${concourse-username} -p ${concourse-password}
 ./fly -t local sync
-./fly -t local set-pipeline -p $5 -c ./source-code/ci/pipeline.yml -n
+./fly -t local set-pipeline -p "hello-world" -c ./source-code/ci/pipeline.yml -n
